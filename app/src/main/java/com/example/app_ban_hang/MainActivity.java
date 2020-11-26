2849ea7d4cc1b630a5740b2ff7fc318b36b715e6
Nguyễn Thanh Hoàng <mr.hoangenglish@gmail.com>
2020-11-26 15:23:10 +0700

    Upload Project

---
+++ app/src/main/java/com/example/app_ban_hang/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.app_ban_hang;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView,lv_item;
+    ArrayAdapter<String> adapter;
+    Toolbar toolbar;
+    SearchView sv;
+    DrawerLayout dr_layout;
+    NavigationView navigationView;
+    Menu menu;
+    String tempt,key,way;
+    ImageView search_volume;
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapping();
+        actionToolBar();
+    }
+    private void mapping(){
+        toolbar = findViewById(R.id.toolbar);
+        dr_layout=findViewById(R.id.drawer_layout);}
+
+    private void actionToolBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.home_icon);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dr_layout.openDrawer(Gravity.START);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+}
